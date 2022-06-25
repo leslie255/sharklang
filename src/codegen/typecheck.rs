0ebef834c7f3d8112047ddab08f046c40f35a927
Leslie Kerman <kerman255@icloud.com>
2022-06-25 17:20:09 +0800

    Cleaner type check code

--- src/codegen/typecheck.rs
+++ src/codegen/typecheck.rs
@@ -1,8 +1,6 @@
 use super::ast::*;
 use super::builtin_funcs::*;
 
-use std::collections::HashMap;
-
 #[allow(unused)]
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub enum DataType {
@@ -37,6 +35,7 @@ impl DataType {
         }
     }
     pub fn size(&self) -> u64 {
+        // in bytes not bits
         match self {
             DataType::UInt8 => 1,
             DataType::UInt16 => 2,
@@ -69,9 +68,8 @@ impl DataType {
     #[allow(unused_variables)]
     pub fn matches(
         &self,
-        ast: &AST,
+        context: &TypeCheckContext,
         expr: &Expression,
-        var_infos: &HashMap<String, VarInfo>,
     ) -> bool {
         match expr {
             Expression::NumberLiteral(_) => match self {
@@ -84,7 +82,7 @@ impl DataType {
                 true
             }
             Expression::Identifier(var_name) => {
-                if let Some(var_info) = var_infos.get(var_name) {
+                if let Some(var_info) = context.parent_block.vars.get(var_name) {
                     &var_info.data_type == self
                 } else {
                     false
@@ -95,53 +93,70 @@ impl DataType {
     }
 }
 
-fn var_exist_check(err_count: &mut usize, var_name: &String, block: &CodeBlock, i: usize) -> bool {
-    if let Some(var_info) = block.vars.get(var_name) {
-        if var_info.def_i > i {
-            *err_count += 1;
+pub struct TypeCheckContext<'a> {
+    pub err_count: &'a mut usize,
+    pub ast: &'a AST,
+    pub parent_block: &'a CodeBlock,
+    pub builtin_fns: &'a BuiltinFuncChecker,
+    pub i: usize,
+}
+impl<'a> TypeCheckContext<'a> {
+    fn new(
+        err_count: &'a mut usize,
+        ast: &'a AST,
+        parent_block: &'a CodeBlock,
+        builtin_fns: &'a BuiltinFuncChecker,
+        i: usize,
+    ) -> TypeCheckContext<'a> {
+        TypeCheckContext {
+            err_count,
+            ast,
+            parent_block,
+            builtin_fns,
+            i,
+        }
+    }
+}
+
+fn var_exist_check(context: &mut TypeCheckContext, var_name: &String) -> bool {
+    if let Some(var_info) = context.parent_block.vars.get(var_name) {
+        if var_info.def_i > context.i {
+            *context.err_count += 1;
             println!("using the variable `{}` before it was declared", var_name);
             return false;
         }
         true
     } else {
-        *err_count += 1;
+        *context.err_count += 1;
         println!("{} is not a variable", var_name);
         false
     }
 }
 
-fn fn_exist_check(
-    err_count: &mut usize,
-    fn_name: &String,
-    ast: &AST,
-    builtin_fns: &BuiltinFuncChecker,
-) -> bool {
-    if ast.func_defs.contains_key(fn_name) {
+fn fn_exist_check(context: &mut TypeCheckContext, fn_name: &String) -> bool {
+    if context.ast.func_defs.contains_key(fn_name) {
         true
-    } else if builtin_fns.is_builtin_func(fn_name) {
+    } else if context.builtin_fns.is_builtin_func(fn_name) {
         true
     } else {
-        *err_count += 1;
+        *context.err_count += 1;
         println!("`{}` is not a function", fn_name);
         false
     }
 }
 
-pub fn fn_args_check(
-    err_count: &mut usize,
-    ast: &AST,
-    block: &CodeBlock,
-    fn_name: &String,
-    input_args: &Vec<usize>,
-) {
-    if !ast.func_defs.contains_key(fn_name) {
+pub fn fn_args_check(context: &mut TypeCheckContext, fn_name: &String, input_args: &Vec<usize>) {
+    if !context.ast.func_defs.contains_key(fn_name) {
         // TODO: arguments check for builtin functions
         return;
     }
-    if let Expression::FuncDef(_, fn_block) = ast.expr(*ast.func_defs.get(fn_name).unwrap()) {
+    if let Expression::FuncDef(_, fn_block) = context
+        .ast
+        .expr(*context.ast.func_defs.get(fn_name).unwrap())
+    {
         // check number of arguments
         if input_args.len() != fn_block.arg_types.len() {
-            *err_count += 1;
+            *context.err_count += 1;
             println!(
                 "expected needs {} arguments for function `{}`, found {}",
                 fn_block.arg_types.len(),
@@ -152,15 +167,15 @@ pub fn fn_args_check(
 
         // check arguments
         for (i, arg_i) in input_args.iter().enumerate() {
-            let arg = ast.expr(*arg_i);
+            let arg = context.ast.expr(*arg_i);
             if let Expression::Identifier(id) = arg {
-                if !var_exist_check(err_count, id, block, *arg_i) {
+                if !var_exist_check(context, id) {
                     continue;
                 }
             }
             let expected_type = fn_block.arg_types.get(i).unwrap();
-            if !expected_type.matches(ast, arg, &block.vars) {
-                *err_count += 1;
+            if !expected_type.matches(context, arg) {
+                *context.err_count += 1;
                 println!(
                     "expected expression of type {} for argument of function `{}`, found {:?}",
                     expected_type.description(),
@@ -182,23 +197,24 @@ pub fn type_check(ast: &AST, builtin_fns: &BuiltinFuncChecker) -> usize {
             Expression::FuncDef(_, block) => {
                 for i in &block.body {
                     let node = ast.node(*i);
+                    let mut context =
+                        TypeCheckContext::new(&mut err_count, ast, block, builtin_fns, *i);
                     match &node.expr {
                         Expression::FuncCall(fn_name, args) => {
-                            if fn_exist_check(&mut err_count, fn_name, ast, builtin_fns) {
-                                fn_args_check(&mut err_count, ast, block, fn_name, args);
-                            }
+                            fn_exist_check(&mut context, fn_name);
+                            fn_args_check(&mut context, fn_name, args);
                         }
                         Expression::VarAssign(var_name, rhs) => {
                             let rhs_expr = ast.expr(*rhs);
-                            if !var_exist_check(&mut err_count, var_name, block, *i) {
+                            if !var_exist_check(&mut context, var_name) {
                                 continue;
                             }
                             if let Expression::FuncCall(fn_name, args) = rhs_expr {
-                                fn_exist_check(&mut err_count, fn_name, ast, builtin_fns);
-                                fn_args_check(&mut err_count, ast, block, fn_name, args);
+                                fn_exist_check(&mut context, fn_name);
+                                fn_args_check(&mut context, fn_name, args);
                             }
                             let lhs_type = &block.vars.get(var_name).unwrap().data_type;
-                            if !lhs_type.matches(ast, rhs_expr, &block.vars) {
+                            if !lhs_type.matches(&context, rhs_expr) {
                                 err_count += 1;
                                 println!(
                                     "expecting expression of type `{}` as rhs, found {:?}",
@@ -210,12 +226,12 @@ pub fn type_check(ast: &AST, builtin_fns: &BuiltinFuncChecker) -> usize {
                         Expression::VarInit(_, var_type, rhs) => {
                             let rhs_expr = ast.expr(*rhs);
                             if let Expression::Identifier(id) = rhs_expr {
-                                var_exist_check(&mut err_count, id, block, *i);
+                                var_exist_check(&mut context, id);
                             } else if let Expression::FuncCall(fn_name, args) = rhs_expr {
-                                fn_exist_check(&mut err_count, fn_name, ast, builtin_fns);
-                                fn_args_check(&mut err_count, ast, block, fn_name, args);
+                                fn_exist_check(&mut context, fn_name);
+                                fn_args_check(&mut context, fn_name, args);
                             }
-                            if !var_type.matches(ast, rhs_expr, &block.vars) {
+                            if !var_type.matches(&context, rhs_expr) {
                                 err_count += 1;
                                 println!(
                                     "expecting expression of type `{}` as rhs, found {:?}",
